7230c28689d452a416f8faf3ce632f5bb42fc930
Tseye2001 <167227608+Tseye2001@users.noreply.github.com>
2024-04-20 18:35:53 -0500

    Update test_television.py

--- test_television.py
+++ test_television.py
@@ -1,86 +1,88 @@
+# test_television.py
 import pytest
 from television import *
+
+
 @pytest.fixture
 def tv():
-   return Television()
+    return Television()
 
 
 def test_init(tv):
-   assert tv._status == False
-   assert tv._muted == False
-   assert tv._volume == Television.MIN_VOLUME
-   assert tv._channel == Television.MIN_CHANNEL
+    assert tv._status == False
+    assert tv._muted == False
+    assert tv._volume == Television._MIN_VOLUME
+    assert tv._channel == Television._MIN_CHANNEL
 
 
 def test_power(tv):
-   tv.power()
-   assert tv._status == True
-   tv.power()
-   assert tv._status == False
+    tv.power()
+    assert tv._status == True
+    tv.power()
+    assert tv._status == False
 
 
 def test_mute(tv):
-   tv.power()  
-   tv.mute()
-   assert tv._muted == True
-   tv.mute()
-   assert tv._muted == False
+    tv.power()
+    tv.mute()
+    assert tv._muted == True
+    tv.mute()
+    assert tv._muted == False
 
 
 def test_channel_up(tv):
-   tv.channel_up()
-   assert tv._channel == Television.MIN_CHANNEL
+    tv.channel_up()
+    assert tv._channel == Television._MIN_CHANNEL
 
-   tv.power()
-   tv.channel_up()
-   assert tv._channel == Television.MIN_CHANNEL + 1
+    tv.power()
+    tv.channel_up()
+    assert tv._channel == Television._MIN_CHANNEL + 1
 
 
 def test_channel_down(tv):
-   tv.power() 
-   tv.channel_down()
-   assert tv._channel == Television.MAX_CHANNEL
-   for _ in range(Television.MAX_CHANNEL - Television.MIN_CHANNEL + 1):
-       tv.channel_down()
-   assert tv._channel == Television.MAX_CHANNEL
+    tv.power()
+    tv.channel_down()
+    assert tv._channel == Television._MAX_CHANNEL
+    for _ in range(Television._MAX_CHANNEL - Television._MIN_CHANNEL + 1):
+        tv.channel_down()
+    assert tv._channel == Television._MAX_CHANNEL
 
 
 def test_volume_up(tv):
-   tv.volume_up()
-   assert tv._volume == Television.MIN_VOLUME
+    tv.volume_up()
+    assert tv._volume == Television._MIN_VOLUME
 
-   tv.power()  
+    tv.power()
 
-   tv.volume_up()
-   assert tv._volume == Television.MIN_VOLUME + 1
+    tv.volume_up()
+    assert tv._volume == Television._MIN_VOLUME + 1
 
-   tv.mute()
-   tv.volume_up()
-   assert tv._volume == Television.MIN_VOLUME + 2
+    tv.mute()
+    tv.volume_up()
+    assert tv._volume == Television._MIN_VOLUME + 1
 
-   for _ in range(Television.MAX_VOLUME - Television.MIN_VOLUME):
-       tv.volume_up()
-   tv.volume_up()
-   assert tv._volume == Television.MAX_VOLUME
+    for _ in range(Television._MAX_VOLUME - Television._MIN_VOLUME):
+        tv.volume_up()
+    tv.volume_up()
+    assert tv._volume == Television._MAX_VOLUME
 
 
 def test_volume_down(tv):
-   
-   tv.volume_down()
-   assert tv._volume == Television.MIN_VOLUME
+    tv.volume_down()
+    assert tv._volume == Television._MIN_VOLUME
 
-   tv.power()  
+    tv.power()
 
-   for _ in range(Television.MAX_VOLUME - Television.MIN_VOLUME):
-       tv.volume_up()
+    for _ in range(Television._MAX_VOLUME - Television._MIN_VOLUME):
+        tv.volume_up()
 
-   tv.volume_down()
-   assert tv._volume == Television.MAX_VOLUME - 1
+    tv.volume_down()
+    assert tv._volume == Television._MAX_VOLUME - 1
 
-   tv.mute()
-   tv.volume_down()
-   assert tv._volume == Television.MAX_VOLUME - 2
+    tv.mute()
+    tv.volume_down()
+    assert tv._volume == Television._MAX_VOLUME - 2
 
-   for _ in range(Television.MAX_VOLUME - Television.MIN_VOLUME + 1): 
-       tv.volume_down()
-   assert tv._volume == Television.MIN_VOLUME
+    for _ in range(Television._MAX_VOLUME - Television._MIN_VOLUME + 1):
+        tv.volume_down()
+    assert tv._volume == Television._MIN_VOLUME
